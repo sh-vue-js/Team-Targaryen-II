8520ecbb2656da5abf050e93fceec726211589e8
Dapo Ola-Olatunji <71828782+Nehemiah01@users.noreply.github.com>
2021-11-29 21:01:49 +0100

    responsive design page, components

---
+++ src/components/AsideTab.vue
@@ -0,0 +1,19 @@
+<template>
+    <aside>
+        <svg class="projecticon" width="38" height="34" viewBox="0 0 118 114" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="project icon">
+            <ellipse id="iconBg" ref="iconBg" cx="44" cy="42" rx="44" ry="42" fill="#ED48BA"/>
+            <ellipse id="Ellipse 66" cx="46.0787" cy="42" rx="25.9843" ry="24.8032" stroke="#B3D4E0" stroke-width="8"/>
+            <ellipse id="Ellipse 67" cx="45.7323" cy="42" rx="9.70079" ry="9.59055" fill="#B3D4E0"/>
+            <ellipse id="Ellipse 68" cx="26.3307" cy="60.8504" rx="9.70079" ry="9.25984" fill="#B3D4E0"/>
+            <ellipse id="Ellipse 69" cx="62.0157" cy="23.1496" rx="10.0472" ry="9.25984" fill="#B3D4E0"/>
+            </g>
+        </svg>
+        <div class="social-media">
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-instagram" aria-hidden="true"></i>
+            <i class="fab fa-vk" aria-hidden="true"></i>
+        </div>
+    </aside>
+                
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+<nav> <!--navabr-->
+    <ul>
+        <li><a href="#"> Home </a></li>
+        <li><a href="#"> About </a></li>
+        <li><a href="#"> Services </a></li>
+        <li><a href="#"> Products  </a></li>
+        <li><a href="#"> Contact </a></li>
+    </ul>
+
+
+    <!--TABLET & MOBILE DISPLAY!! -->
+    
+    <div @click="toggleCancel" ref='ham' id="hamburger">
+        <span></span>
+        <span></span>  
+    </div>
+    
+    <a href="#">
+        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+    </a>
+</nav>
+</template>
+
+<script>
+
+export default {
+    methods: {
+        toggleCancel() {
+            this.$refs.ham.classList.toggle('cancel')
+        }
+    }
+}
+
+</script>
